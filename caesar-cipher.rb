d2ad76effe5a3e90614399adcd1c540910b3ede1
Eero Ruosteenoja <eero.ruosteenoja@gmail.com>
2025-01-12 22:52:42 +0200

    Complete caesar-cipher

---
+++ caesar-cipher.rb
@@ -0,0 +1,13 @@
+def caesar_cipher(string, shift)
+  crypted_string = ""
+  string.split("").each do |chr|
+    if chr.ord >= 65 && chr.ord <= 90
+      chr = (65+(chr.ord+shift-65)%26).chr
+    elsif chr.ord >= 97 && chr.ord <= 122
+      chr = (97+(chr.ord+shift-97)%26).chr
+    end
+    crypted_string += chr
+  end
+  crypted_string
+end
+
